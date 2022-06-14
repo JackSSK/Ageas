d104497b4331474595181085cc795a654ecc089d
JackSSK <angrayg@gmail.com>
2022-06-14 15:04:53 +0800

    Update _visualizer.py
    I would leave it here for now

--- ageas/_visualizer.py
+++ ageas/_visualizer.py
@@ -9,6 +9,7 @@ import numpy as np
 import networkx as nx
 from warnings import warn
 import matplotlib as mpl
+import matplotlib.cm as cmx
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 import matplotlib.colors as colors
@@ -89,42 +90,51 @@ class Plot_Regulon(object):
                 # add id to dict accordingly
                 if id not in dict and self.__check(regulon.grps[id].type):
                     dict[id] = None
-                # recurssion
+                # call self recurssionly
                 self.__find_grps(regulon, tar, dict, depth)
 
     # make sure which GRPs are qualified and plot them
     def draw(self,
              scale:int = 1,
              seed:int = 1936,
-             figure_size:int = 10,
+             node_base_size:int = 2,
+             figure_size:int = 20,
              method:str = 'netgraph',
+             legend_size:int = 10,
              layout:str = 'spring',
              colorbar_shrink:float = 0.25,
-             font_size:int = 5,
+             font_size:int = 10,
              hide_target_labels:bool = False,
-             edge_width_scale:float = 1.0,
-             bridge_color:str = None,
+             edge_width_scale:float = 4.0,
              save_path:str = None):
         # initialization
         self.node_cmap = plt.cm.Set3
         self.edge_cmap = plt.cm.coolwarm
-        plt.figure(figsize = (figure_size, figure_size))
+
+        # Color mapping
+        self.edge_scalar_map = cmx.ScalarMappable(
+            norm = colors.Normalize(vmin = -1, vmax = 1, clip = True),
+            cmap = self.edge_cmap
+        )
+        self.node_scalar_map = cmx.ScalarMappable(
+            norm = colors.Normalize(vmin = 0, vmax = 1, clip = True),
+            cmap = self.node_cmap
+        )
+
+        fig = plt.figure(figsize = (figure_size, figure_size))
         ax = plt.gca()
         ax.set_axis_off()
 
-        # if removing all the bridges, why specify a color?
-        if self.hide_bridge and bridge_color is not None:
-            warn('bridge_color ignored since hiding bridges')
         # draw with specified method
         if method == 'networkx':
             self.draw_with_networkx(
                 ax = ax,
                 scale = scale,
                 layout = layout,
+                base_size = node_base_size,
                 font_size = font_size,
                 hide_target_labels = hide_target_labels,
                 edge_width_scale =edge_width_scale,
-                bridge_color = bridge_color
             )
         elif method == 'netgraph':
             self.draw_with_netgraph(
@@ -132,37 +142,45 @@ class Plot_Regulon(object):
                 scale = scale,
                 seed = seed,
                 layout = layout,
+                base_size = node_base_size,
                 font_size = font_size,
                 hide_target_labels = hide_target_labels,
                 edge_width_scale = edge_width_scale,
-                bridge_color = bridge_color
             )
-        self.set_color_bar(ax = ax, shrink = colorbar_shrink)
+
+        # add color bar and legend
+        self.set_legend(
+            legend_size = legend_size,
+            method = method
+        )
+        self.set_color_bar(
+            ax = ax,
+            shrink = colorbar_shrink,
+            font_size = font_size
+        )
+        fig.tight_layout()
+
         if save_path is not None:
             self.save(save_path)
 
     # Netgraph plot method
     def draw_with_netgraph(self,
                            ax = plt.gca(),
-                           base_size:int = 1,
-                           base_alpha:float = 0.3,
+                           base_size:int = 2,
                            scale:int = 1,
                            seed:int = 1936,
                            layout:str = 'spring',
                            font_size:int = 5,
                            hide_target_labels:bool = False,
-                           edge_width_scale:float = 0.1,
-                           bridge_color:str = None,):
+                           edge_width_scale:float = 0.1,):
         node_size, node_color, node_alhpa, node_labels = self.get_node_info(
             base_size = base_size,
             color_type = 'rgba',
             hide_target_labels = hide_target_labels,
         )
         edge_width, edge_style, edge_color, edge_alpha = self.get_edge_info(
-            base_alpha = base_alpha,
             width_scale = edge_width_scale,
             color_type = 'rgba',
-            bridge_color = bridge_color,
         )
         node_shape = {node:'o' for node in self.graph.nodes}
         for node, data in self.graph.nodes(data = True):
@@ -185,29 +203,22 @@ class Plot_Regulon(object):
             arrows = True,
         )
 
-        # # set color bar
-        # plt.colorbar(ax = ax)
-
     # Networkx plot method
     def draw_with_networkx(self,
                            ax = plt.gca(),
                            base_size:int = 600,
-                           base_alpha:float = 0.3,
                            scale:int = 1,
                            seed:int = 1914,
                            layout:str = 'spring',
                            font_size:int = 5,
                            hide_target_labels:bool = False,
-                           edge_width_scale:float = 1.0,
-                           bridge_color:str = None,):
+                           edge_width_scale:float = 1.0,):
         node_size, node_color, node_alhpa, node_labels = self.get_node_info(
             base_size = base_size,
             hide_target_labels = hide_target_labels,
         )
         edge_width, edge_style, edge_color, edge_alpha = self.get_edge_info(
-            base_alpha = base_alpha,
             width_scale = edge_width_scale,
-            bridge_color = bridge_color,
         )
         node_size = [node_size[node] for node in self.graph.nodes]
         node_color = [node_color[node] for node in self.graph.nodes]
@@ -271,10 +282,8 @@ class Plot_Regulon(object):
 
     # Get Edge Information
     def get_edge_info(self,
-                      base_alpha = 0.3,
                       width_scale = 1,
                       color_type = 'int',
-                      bridge_color = None,
                       return_type = 'dict'):
         edge_width = dict()
         edge_style = dict()
@@ -283,38 +292,29 @@ class Plot_Regulon(object):
         for (source, target, data) in self.graph.edges(data = True):
             key = (source, target)
             # set edge width
-            edge_width[key] = max(abs(data['weight']) * 5, 1) * width_scale
+            edge_width[key] = abs(data['weight']) * width_scale
             # set info by type
             if data['type'] == TYPES[2]:
                 edge_style[key] = ':'
-                edge_alpha[key] = base_alpha
-                if bridge_color is not None:
-                    edge_color[key] = bridge_color
-                    continue
             else:
                 edge_style[key] = '-'
-                edge_alpha[key] = min(
-                    1,
-                    base_alpha + abs(data['weight'])
-                )
+            # set up alpha value
+            edge_alpha[key] = min(1, abs(data['weight']))
             # set color
             if data['weight'] < 0:
-                if color_type == 'int':
-                    edge_color[key] = -1
-                elif color_type == 'rgba':
-                    edge_color[key] = self.edge_cmap(-1)
+                edge_color[key] = -1
             else:
-                if color_type == 'int':
-                    edge_color[key] = 1
-                elif color_type == 'rgba':
-                    edge_color[key] = self.edge_cmap(1)
+                edge_color[key] = 1
+            # change color to rgba format if specified
+            if color_type == 'rgba':
+                edge_color[key] = self.edge_scalar_map.to_rgba(edge_color[key])
 
         return edge_width, edge_style, edge_color, edge_alpha
 
 
     # Get Node information
     def get_node_info(self,
-                      base_size = 600,
+                      base_size = 800,
                       hide_target_labels = False,
                       color_type = 'int',
                       return_type = 'dict'):
@@ -327,20 +327,17 @@ class Plot_Regulon(object):
             # target_num = len([i for i in self.graph.successors(node)])
             target_num = len(data['target'])
             if target_num > 0:
-                if color_type == 'int':
-                    node_color[node] = 1
-                elif color_type == 'rgba':
-                    node_color[node] = self.node_cmap(1)
+                node_color[node] = 1
                 # increase node size according to gene's reg power
                 if target_num > 10:
                     factor = math.log10(target_num) * 2
             else:
-                if color_type == 'int':
-                    node_color[node] = 0
-                elif color_type == 'rgba':
-                    node_color[node] = self.node_cmap(0)
+                node_color[node] = 0
                 if hide_target_labels:
                     del node_labels[node]
+            # change color to rgba format if specified
+            if color_type == 'rgba':
+                node_color[node]=self.node_scalar_map.to_rgba(node_color[node])
             size = base_size * factor
             node_size[node] = size
 
@@ -357,13 +354,8 @@ class Plot_Regulon(object):
         return weight
 
     # Set up a color bar with fixed scale from -1.0 to 1.0
-    def set_color_bar(self, ax, shrink = 1):
-        pc = img.AxesImage(
-            ax = ax,
-            cmap = self.edge_cmap,
-            norm = colors.Normalize(vmin = -1, vmax = 1, clip = True),
-        )
-        cbar = plt.colorbar(pc, ax = ax, shrink = shrink)
+    def set_color_bar(self, ax, shrink = 1, font_size = 10):
+        cbar = plt.colorbar(self.edge_scalar_map, ax = ax, shrink = shrink)
         if self.graph_type == 'all':
             labels = cbar.ax.get_yticklabels()
             labels[0] = 'Stronger in Class2'
@@ -372,11 +364,60 @@ class Plot_Regulon(object):
                 labels[int(len(labels) / 2)] = 'No Difference'
             labels[-1] = 'Stronger in Class1'
             cbar.set_ticklabels(labels)
+            cbar.ax.tick_params(labelsize = font_size)
         cbar.ax.set_ylabel(
             'Gene Expression Correlation',
+            fontsize = font_size,
+            fontweight = 'bold',
             labelpad = 12.0,
             rotation = 270
         )
+
+    # Set Up Legend
+    def set_legend(self, legend_size, method):
+        plt.scatter(
+            [],[],
+            s = legend_size * 10,
+            marker = 'o',
+            c = [self.node_cmap(0)],
+            label = 'Regulatory Target'
+        )
+        plt.scatter(
+            [],[],
+            s = legend_size * 10,
+            marker = 'o',
+            c = [self.node_cmap(1)],
+            label = 'Regulatory Source'
+        )
+        if method == 'netgraph':
+            plt.scatter(
+                [],[],
+                s = legend_size * 10,
+                marker = 'd',
+                c = ['black'],
+                label = 'TF'
+            )
+            plt.scatter(
+                [],[],
+                s = legend_size * 10,
+                marker = 'o',
+                c = ['black'],
+                label = 'Gene'
+            )
+        elif method == 'networkx':
+            plt.plot(
+                [], [],
+                linestyle = 'dashed',
+                c = 'black',
+                label = 'Bridge GRP'
+            )
+            plt.plot(
+                [], [],
+                linestyle = 'solid',
+                c = 'black',
+                label = 'Key GRP'
+            )
+        plt.legend(loc = 1, prop = {'size': legend_size})
 
     # save the plot. PDF by default
     def save(self, path:str = None, format:str = 'pdf'):
@@ -389,9 +430,6 @@ class Plot_Regulon(object):
 
 
 if __name__ == '__main__':
-    import ageas
-
-
     header = 'liverCCl4/hsc_pf_a6w/'
     for i in range(1):
         folder_path = header + 'run_' + str(i) + '/'
@@ -399,32 +437,9 @@ if __name__ == '__main__':
         a = Plot_Regulon(
             file_path = atlas_path,
             regulon_id = 'regulon_0',
-            # hide_bridge = False,
-            # graph_type = 'class2',
+            hide_bridge = False,
+            graph_type = 'class2',
             root_gene = 'HAND2',
             # impact_depth = 1,
         )
-        a.draw(
-            figure_size = 10,
-            colorbar_shrink = 0.25,
-            font_size = 10,
-            edge_width_scale = 0.3,
-            save_path = folder_path + 'ultimate_useless_mess.pdf',
-        )
-        a.draw(
-            method = 'networkx',
-            figure_size = 20,
-            font_size = 10,
-            save_path = folder_path + 'ultimate_useless_mess.pdf',
-        )
-
-        # a = ageas.Plot_Regulon(
-        #     figure_size = 10,
-        #     file_path = atlas_path,
-        #     regulon_id = 'regulon_0',
-        #     # hide_bridge = False,
-        #     # bridge_special_color = 'silver',
-        #     # type = 'class1',
-        #     # root_gene = 'ACTA2',
-        #     # impact_depth = 1,
-        # )
+        a.draw(save_path = folder_path + 'ultimate_useless_mess.pdf',)
