fa4df22eb6f7be427b1ccf607aca46242a969ed4
JackSSK <angrayg@gmail.com>
2022-06-14 15:41:30 +0800

    Copyright related stuffs

--- ageas/__init__.py
+++ ageas/__init__.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 
 
-# Copyright (C) 2016 Jack Yu <gyu17@jh.edu>
+# Copyright (C) 2022 Jack Yu <gyu17@jh.edu>
+# Author: Jack Yu <gyu17@jh.edu> <Itself Tech inc.>
+# Author: Masayoshi Nakamoto <nkmtmsys@gmail.com> <Itself Tech inc.>
 
-# Author: Jack Yu <gyu17@jh.edu>
-# Author: Masayoshi Nakamoto <nkmtmsys@gmail.com>
-
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
+# This program is free software
+# You can redistribute it and/or modify it
+# under the terms of the GNU General Public License
+# as published by the Free Software Foundation;
+# either version 3 of the License, or (at your option) any later version.
 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
